016597c1aa950fc25371c44a3e031ffbd8bfef5a
Nikunj Khanna <nikunj.kkhanna@gmail.com>
2025-05-06 10:12:05 +0000

    commit

--- models/marts/core/mart_weatherimpact.sql
+++ models/marts/core/mart_weatherimpact.sql
@@ -1,8 +1,22 @@
+select trip_date,
+    trip_year,
+    trip_month,
+    weather_main,
+    weather_description,
+    total_trips
+    from(
+
 SELECT 
     trip_date,
     trip_year,
     trip_month,
     weather_main,
     weather_description,
-    temperature
+    count(*) as total_trips
     FROM {{ ref('int_bike_weather') }}
+    group by trip_date,
+    trip_year,
+    trip_month,
+    weather_main,
+    weather_description )
+    
