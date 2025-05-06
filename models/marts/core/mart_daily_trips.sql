7f8ace7d8825a27ab78730704000a9982c00f054
Nikunj Khanna <nikunj.kkhanna@gmail.com>
2025-05-06 09:36:56 +0000

    commit

--- models/marts/core/mart_daily_trips.sql
+++
@@ -1,6 +0,0 @@
-select trip_date,trip_year,trip_month,
-count(*) as total_trips,
-count(distinct user_type) as user_types,
-avg(temperature) as avg_temp
-from {{ref("int_bike_weather")}}
-group by trip_date,trip_year,trip_month
--- models/marts/core/mart_weatherimpact.sql
+++ models/marts/core/mart_weatherimpact.sql
@@ -1,15 +1,8 @@
 SELECT 
-    trip_date,trip_year,trip_month,
+    trip_date,
+    trip_year,
+    trip_month,
     weather_main,
     weather_description,
-    AVG(total_trips) AS avg_trips
-FROM (
-    SELECT 
-        trip_date,trip_year,trip_month,
-        weather_main,
-        weather_description,
-        COUNT(*) AS total_trips
-    FROM {{ ref('int_bike_weather') }}
-    GROUP BY trip_date, weather_main, weather_description,trip_year,trip_month
-)
-GROUP BY trip_date, weather_main, weather_description,trip_year,trip_month
+    temperature
+    FROM {{ ref('int_bike_weather') }};
