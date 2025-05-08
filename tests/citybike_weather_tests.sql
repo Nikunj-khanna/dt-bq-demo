8aa876132a6e432e62ef4cf5064f395d4247efe7
Nikunj Khanna <nikunj.kkhanna@gmail.com>
2025-05-08 04:08:13 +0000

    updated test.sql

--- tests/citybike_weather_tests.sql
+++ tests/citybike_weather_tests.sql
@@ -1,19 +1,18 @@
+
 select 
     count(*) as num_null_trip_durations
 from {{ ref('stg_public_citybikes') }}
 where trip_duration is null
-having count(*) > 0;
-
+ union all
 
 select 
     count(*) as num_negative_trip_durations
 from {{ ref('stg_public_citybikes') }}
 where trip_duration < 0
-having count(*) > 0;
 
-
+union all
 select 
     count(*) as num_null_or_zero_bike_ids
 from {{ ref('stg_public_citybikes') }}
 where bike_id is null or bike_id = 0
-having count(*) > 0;
+
