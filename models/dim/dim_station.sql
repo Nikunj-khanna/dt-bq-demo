2026dbc18e7d9046a34145a22f53e0b57edf41c8
Nikunj Khanna <nikunj.kkhanna@gmail.com>
2025-05-08 04:41:26 +0000

    updated dim_station and mart_stationweather

--- models/dim/dim_station.sql
+++ models/dim/dim_station.sql
@@ -1,6 +1,5 @@
-WITH start_stations AS (
-
-    SELECT DISTINCT
+WITH all_stations AS (
+    SELECT 
         start_station_id AS station_id,
         start_station_name AS station_name,
         start_station_latitude AS latitude,
@@ -8,26 +7,27 @@ WITH start_stations AS (
     FROM {{ ref('stg_public_citybikes') }}
     WHERE start_station_id IS NOT NULL
 
-),
+    UNION ALL
 
-end_stations AS (
-
-    SELECT DISTINCT
+    SELECT 
         end_station_id AS station_id,
         end_station_name AS station_name,
         end_station_latitude AS latitude,
         end_station_longitude AS longitude
     FROM {{ ref('stg_public_citybikes') }}
     WHERE end_station_id IS NOT NULL
-
 ),
 
-all_stations AS (
-
-    SELECT * FROM start_stations
-    UNION
-    SELECT * FROM end_stations
-
+ranked AS (
+    SELECT *,
+        ROW_NUMBER() OVER (PARTITION BY station_id ORDER BY station_name) AS row_num
+    FROM all_stations
 )
 
-SELECT DISTINCT * FROM all_stations
+SELECT
+    station_id,
+    station_name,
+    latitude,
+    longitude
+FROM ranked
+WHERE row_num = 1
